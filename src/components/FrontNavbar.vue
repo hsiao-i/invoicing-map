b19c9a29ac50e479f6f897cf382d1ec001216bce
hsiao-i <eva090029@gmail.com>
2023-11-20 23:13:58 +0800

    feat (#01-map): Create navbar Component and delete useless pages

---
+++ src/components/FrontNavbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container mx-auto">
+    <div class="flex justify-between py-5">
+
+        <h1>
+          <RouterLink to="/">
+            2020 總統大選開票地圖
+          </RouterLink>
+        </h1>
+        <nav>
+          <!-- <RouterLink to="/">Home</RouterLink> -->
+          <RouterLink to="/voteMap">開票地圖</RouterLink>
+        </nav>
+
+    </div>
+  </div>
+</template>
